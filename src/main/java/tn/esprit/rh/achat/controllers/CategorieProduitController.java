2aeb0bc36d9c40f44ae7099c9ee402034b4b2cc8
Nour Elhouda Zemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-11-03 17:01:57 +0100

    Update CategorieProduitController.java

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -3,6 +3,7 @@ package tn.esprit.rh.achat.controllers;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.services.ICategorieProduitService;
 

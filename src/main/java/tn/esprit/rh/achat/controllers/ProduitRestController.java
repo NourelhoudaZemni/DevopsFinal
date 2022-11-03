190edda8caadd3ebd425554724060a1641acfb87
Nour Elhouda Zemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-11-03 17:03:50 +0100

    Update ProduitRestController.java

--- src/main/java/tn/esprit/rh/achat/controllers/ProduitRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/ProduitRestController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.services.IProduitService;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 

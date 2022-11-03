68d3ec5774693b53fa8c89b213d7372e7bc0ec11
Nour Elhouda Zemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-11-03 17:02:46 +0100

    Update SecteurActiviteController.java

--- src/main/java/tn/esprit/rh/achat/controllers/SecteurActiviteController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/SecteurActiviteController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.rh.achat.entities.SecteurActivite;
 import tn.esprit.rh.achat.services.ISecteurActiviteService;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @RestController
